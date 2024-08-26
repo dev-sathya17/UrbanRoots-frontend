951261b6a37c92601c6cc7209f5929a703e167be
Sathyanarayanan V <dev.sathya1701@gmail.com>
2024-08-26 13:32:16 +0530

    complete signup functionality

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,84 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import service from "../services/user.service";
+
 const SignUp = () => {
-  return <div>SignUp</div>;
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    setLoading(true);
+    e.preventDefault();
+    const { status, data } = await service.signUp(formData);
+    if (status === 201) {
+      alert(data.message);
+      setFormData({
+        username: "",
+        email: "",
+        password: "",
+      });
+      setError(null);
+      navigate("/sign-in");
+    } else {
+      setError(data.message);
+    }
+    setLoading(false);
+  };
+
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Username"
+          className="border p-3 rounded-lg"
+          id="username"
+          onChange={handleChange}
+          value={formData.username}
+        />
+        <input
+          type="email"
+          placeholder="email"
+          className="border p-3 rounded-lg"
+          id="email"
+          onChange={handleChange}
+          value={formData.email}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="border p-3 rounded-lg"
+          id="password"
+          onChange={handleChange}
+          value={formData.password}
+        />
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Sign Up"}
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Have an account?</p>
+        <Link to="/sign-in">
+          <span className="text-blue-700">Sign In</span>
+        </Link>
+      </div>
+      {error && <p className="text-red-500 mt-5">{error}</p>}
+    </div>
+  );
 };
 
 export default SignUp;
---
+++ src/services/user.service.js
@@ -0,0 +1,20 @@
+import { instance } from "./instance";
+
+const service = {
+  signUp: async (data) => {
+    try {
+      const response = await instance.post("/users/signup", data);
+      return {
+        status: response.status,
+        data: response.data,
+      };
+    } catch (error) {
+      return {
+        status: error.response.data.statusCode,
+        data: error.response.data,
+      };
+    }
+  },
+};
+
+export default service;
