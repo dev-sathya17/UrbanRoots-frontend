306824142303a0cb1804e81e659d5bd77536ef36
Sathyanarayanan V <dev.sathya1701@gmail.com>
2024-08-28 19:31:10 +0530

    complete signin page functionality

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,74 @@
-const Signin = () => {
-  return <div>Signin</div>;
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import service from "../services/user.service";
+
+const SignIn = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    setLoading(true);
+    e.preventDefault();
+    const { status, data } = await service.signIn(formData);
+    if (status === 200) {
+      alert("Logged in successfully");
+      setFormData({
+        email: "",
+        password: "",
+      });
+      setError(null);
+      navigate("/");
+    } else {
+      setError(data.message);
+    }
+    setLoading(false);
+  };
+
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="email"
+          className="border p-3 rounded-lg"
+          id="email"
+          onChange={handleChange}
+          value={formData.email}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="border p-3 rounded-lg"
+          id="password"
+          onChange={handleChange}
+          value={formData.password}
+        />
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Sign In"}
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Dont Have an account?</p>
+        <Link to="/sign-up">
+          <span className="text-blue-700">Sign Up</span>
+        </Link>
+      </div>
+      {error && <p className="text-red-500 mt-5">{error}</p>}
+    </div>
+  );
 };
 
-export default Signin;
+export default SignIn;
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -15,6 +15,22 @@ const service = {
       };
     }
   },
+  signIn: async (data) => {
+    try {
+      const response = await instance.post("/users/signin", data, {
+        withCredentials: true,
+      });
+      return {
+        status: response.status,
+        data: response.data,
+      };
+    } catch (error) {
+      return {
+        status: error.response.data.statusCode,
+        data: error.response.data,
+      };
+    }
+  },
 };
 
 export default service;
