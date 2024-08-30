a80d1b7cf9527e8150bcd81ae8e1f27f312c12dc
Sathyanarayanan V <dev.sathya1701@gmail.com>
2024-08-30 14:38:26 +0530

    create private routes --modify header to include profile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,9 @@
 import { FaSearch } from "react-icons/fa";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 const Header = () => {
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -29,10 +32,18 @@ const Header = () => {
               About
             </li>
           </Link>
-          <Link to={"sign-in"}>
-            <li className="hidden sm:inline text-slate-700 hover:underline">
-              Sign In
-            </li>
+          <Link to={currentUser ? "/profile" : "/sign-in"}>
+            {currentUser ? (
+              <img
+                src={currentUser.avatar}
+                className="rounded-full h-8 w-8 object-cover"
+                alt="profile picture"
+              />
+            ) : (
+              <li className="hidden sm:inline text-slate-700 hover:underline">
+                Sign In
+              </li>
+            )}
           </Link>
         </ul>
       </div>
